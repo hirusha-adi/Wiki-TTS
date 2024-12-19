66615ce85ce2ff1d32b541317c089c4de9bbb548
Hirusha Adikari <hirushaadi@gmail.com>
2024-12-19 21:59:49 +0530

    URL chooser

---
+++ src/components/ChangeMode.jsx
@@ -0,0 +1,43 @@
+import PropTypes from "prop-types";
+
+const ChangeMode = ({ useUrl, setUseUrl }) => {
+  return (
+    <>
+      <div className="flex justify-between items-center mx-10">
+        <div className="form-control">
+          <label className="label cursor-pointer">
+            <input
+              type="radio"
+              name="option"
+              value="url"
+              className="radio checked:bg-blue-500"
+              checked={useUrl}
+              onChange={() => setUseUrl(true)}
+            />
+            <span className="label-text ml-2">Use URL</span>
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="label cursor-pointer">
+            <input
+              type="radio"
+              name="option"
+              value="text"
+              className="radio checked:bg-blue-500"
+              checked={!useUrl}
+              onChange={() => setUseUrl(false)}
+            />
+            <span className="label-text ml-2">Custom Text</span>
+          </label>
+        </div>
+      </div>
+    </>
+  );
+};
+
+ChangeMode.propTypes = {
+  useUrl: PropTypes.bool.isRequired,
+  setUseUrl: PropTypes.func.isRequired,
+};
+
+export { ChangeMode };
