f24a356884a6c861646a298589de7903aaec4a2f
Hirusha Adikari <hirushaadi@gmail.com>
2024-12-19 21:59:39 +0530

    basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import './App.css'
+import "./App.css";
+import { useState } from "react";
+import { ChangeMode } from "./components/ChangeMode";
 
 function App() {
+  const [useUrl, setUseUrl] = useState(true);
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <div className="mt-5">
+        <div className="text-center text-2xl font-bold">Wiki TTS</div>
+        <div className="bg-base-200 mx-3 my-5 min-h-[80vh] rounded-2xl py-5 px-5">
+          <ChangeMode useUrl={useUrl} setUseUrl={setUseUrl} />
+
+          {useUrl ? <></> : <></>}
+          <textarea
+            className="textarea textarea-info"
+            placeholder="Bio"
+          ></textarea>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
