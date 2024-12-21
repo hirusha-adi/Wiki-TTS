d19fa21c343d668b726c5b58667181ab8a96b05f
Hirusha Adikari <hirushaadi@gmail.com>
2024-12-21 13:19:53 +0530

    Update FromUrl.jsx

--- src/components/FromUrl.jsx
+++ src/components/FromUrl.jsx
@@ -1,30 +1,53 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 const FromUrl = ({ lookupUrl, setLookupUrl }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSetLookupUrl = () => {
+    setLookupUrl(inputValue);
+  };
+
+  const handleClearLookupUrl = () => {
+    setInputValue("");
+    setLookupUrl("");
+  };
+
   return (
     <>
       <div className="w-full">
         <div className="flex flex-wrap gap-4">
           <input
             type="text"
-            placeholder="Type here"
+            placeholder="Enter the url here..."
             className="input input-bordered w-full flex-[4_0_100%]"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
           />
-          <div className="flex w-full">
-            <button className="btn btn-primary flex-[3_1_auto]">Primary</button>
-            <button className="btn btn-error text-white flex-[1_1_auto]">
-              X
+          <div className="flex w-full gap-4">
+            <button
+              className="btn btn-primary flex-[3_1_auto]"
+              onClick={handleSetLookupUrl}
+            >
+              Search
+            </button>
+            <button
+              className="btn btn-error text-white flex-[1_1_auto]"
+              onClick={handleClearLookupUrl}
+            >
+              Clear
             </button>
           </div>
         </div>
+        {lookupUrl}
       </div>
     </>
   );
 };
 
 FromUrl.propTypes = {
-  useUrl: PropTypes.bool.isRequired,
-  setUseUrl: PropTypes.func.isRequired,
+  lookupUrl: PropTypes.string.isRequired,
+  setLookupUrl: PropTypes.func.isRequired,
 };
 
 export { FromUrl };
